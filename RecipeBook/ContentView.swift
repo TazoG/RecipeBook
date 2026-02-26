aafb65664587a6b3d17b6f10b2dbcc5426005e8a
Tazo <tazogigitashvili@gmail.com>
2026-02-26 22:16:46 +0400

    Initial commit

---
+++ RecipeBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeBook
+//
+//  Created by Tazo Gigitashvili on 26.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeBook/RecipeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookApp.swift
+//  RecipeBook
+//
+//  Created by Tazo Gigitashvili on 26.02.26.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
